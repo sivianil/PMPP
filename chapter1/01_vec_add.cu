1d72f5ae5ba06a10db5a7eb33549770065e56be7
Anil Boddapati <anilkumarboddapati@gmail.com>
2026-07-30 22:26:27 +0530

    Vector Addition CUDA

---
+++ chapter1/01_vec_add.cu
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <math.h>
+// Kernel function to add the elements of two arrays
+__global__ void add(int n, float *x, float *y)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = index; i < n; i += stride)
+        y[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+    int N = 1 << 20;
+    float *x, *y;
+
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++)
+    {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // Run the kernel
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    // add<<<1, blockSize>>>(N, x, y);
+    add<<<numBlocks, blockSize>>>(N, x, y); // Launch the kernel with enough blocks to cover all elements
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
