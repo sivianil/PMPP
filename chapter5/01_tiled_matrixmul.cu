26a989d5a2849e28da2a9287627f787b7c7ddeaf
Anil Boddapati <anilkumarboddapati@gmail.com>
2026-08-03 21:54:31 +0530

    Efficient Matrix Multiplication by Tiling

--- chapter5/01_tiled_matrixmul.cu
+++ chapter5/01_tiled_matrixmul.cu
@@ -1,7 +1,23 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-__global__ void MatrixMulKernel(float *Md, float *Nd, float *Pd, int Width)
+__global__ void MatrixMulKernel(float *Md, float *Nd, float *Pd, int m, int n, int o)
+{
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (row < m && col < o)
+    {
+        float Pvalue = 0;
+        for (int k = 0; k < n; k++)
+        {
+            Pvalue += Md[row * n + k] * Nd[k * o + col];
+        }
+        Pd[row * o + col] = Pvalue;
+    }
+}
+
+__global__ void TiledMatrixMulKernel(float *Md, float *Nd, float *Pd, int m, int n, int o)
 {
     // Declare shared memory variables
     __shared__ float Mds[TILE_WIDTH][TILE_WIDTH];
@@ -20,19 +36,81 @@ __global__ void MatrixMulKernel(float *Md, float *Nd, float *Pd, int Width)
     float Pvalue = 0;
     // Iterate over the Md, Nd tiles to compute the Pd element
     // No of iterations = No of Phases = Width / TILE_WIDTH
-    for (int m = 0; m < Width / TILE_WIDTH; m++)
+    for (int m = 0; m < n / TILE_WIDTH; m++)
     {
-        // Threads in collaboration to load Md & Nd elements by accessing global memory once into shared memory
-        Mds[ty][tx] = Md[Row * Width + (m * TILE_WIDTH + tx)];
-        Mds[ty][tx] = Md[(m * TILE_WIDTH + ty) * Width + Col];
+        if (Row < m && (m * TILE_WIDTH + tx) < n)
+        {
+            // Threads in collaboration to load Md & Nd elements by accessing global memory once into shared memory
+            Mds[ty][tx] = Md[Row * n + (m * TILE_WIDTH + tx)];
+        }
+        else
+        {
+            Mds[ty][tx] = 0.0f; // Padding with 0.0f for threads that are out of bounds
+        }
+
+        if ((m * TILE_WIDTH + ty) < n && Col < o)
+        {
+            Nds[ty][tx] = Nd[(m * TILE_WIDTH + ty) * o + Col];
+        }
+        else
+        {
+            Nds[ty][tx] = 0.0f;
+        }
         __syncthreads(); // to make sure that all threads in a block have finished the loading Md & Nd tiles unto Mds, Nds
+
+        for (int k = 0; k < TILE_WIDTH; k++)
+        {
+            Pvalue += Mds[ty][k] * Nds[k][tx];
+            __syncthreads(); // All threads of the block have completed using Mds, Nds contents before loops into the next iteration and loads the next Md, Nd tiles
+        }
     }
 
-    for (int k = 0; k < Width; k++)
-    {
-        Pvalue += Mds[ty][k] * Nds[k][tx];
-        __syncthreads(); // All threads of the block have completed using Mds, Nds contents before loops into the next iteration and loads the next Md, Nd tiles
-    }
+    Pd[Row * o + Col] = Pvalue;
+}
 
-    Pd[Row * Width + Col] = Pvalue;
+void MatrixMul(float *M, float *N, float *P, int m, int n, int o)
+{
+    float *Md, *Nd, *Pd;
+
+    cudaMalloc((void **)&Md, m * n * sizeof(float));
+    cudaMalloc((void **)&Nd, n * o * sizeof(float));
+    cudaMalloc((void **)&Pd, m * o * sizeof(float));
+
+    cudaMemcpy(Md, M, m * n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(Nd, N, n * o * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 dimBlock(TILE_WIDTH, TILE_WIDTH);
+    dim3 dimGrid((o + dimBlock.x - 1) / dimBlock.x, (m + dimBlock.y - 1) / dimBlock.y);
+
+    MatrixMulKernel<<<dimGrid, dimBlock>>>(Md, Nd, Pd, m, n, o);
+
+    cudaMemcpy(P, Pd, m * o * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(Md);
+    cudaFree(Nd);
+    cudaFree(Pd);
+}
+
+void MatrixMulTiling(float *M, float *N, float *P, int m, int n, int o)
+{
+
+    float *Md, *Nd, *Pd;
+
+    cudaMalloc((void **)&Md, m * n * sizeof(float));
+    cudaMalloc((void **)&Nd, n * o * sizeof(float));
+    cudaMalloc((void **)&Pd, m * o * sizeof(float));
+
+    cudaMemcpy(Md, M, m * n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(Nd, N, n * o * sizeof(float), cudaMemcpyHostToDevice);
+
+    dim3 dimBlock(TILE_WIDTH, TILE_WIDTH);
+    dim3 dimGrid((o + dimBlock.x - 1) / dimBlock.x, (m + dimBlock.y - 1) / dimBlock.y);
+
+    MatrixMulKernel<<<dimGrid, dimBlock>>>(Md, Nd, Pd, m, n, o);
+
+    cudaMemcpy(P, Pd, m * o * sizeof(float), cudaMemcpyDeviceToHost);
+
+    cudaFree(Md);
+    cudaFree(Nd);
+    cudaFree(Pd);
 }
